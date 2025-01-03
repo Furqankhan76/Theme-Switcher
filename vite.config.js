ae070c8372f95e20005b46bb11061773197dd978
furqan <furqankhanfk0999@gmail.com>
2025-01-03 12:28:39 +0530

    c

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Theme-Switcher", // Replace 'my-react-app' with your actual repo name
+  base: "/Theme-Switcher/", // Replace 'my-react-app' with your actual repo name
 });
